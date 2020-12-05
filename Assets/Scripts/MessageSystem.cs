93ba7e8c3139b1b2ddb0a7c77ddbea50ed24f05b
Kaktys Koder <kaktyskoder@gmail.com>
2020-12-05 22:37:04 +0400

    Learn Message System

---
+++ Assets/Scripts/MessageSystem.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class MessageSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject testObject;
+
+    /// <summary>
+    /// Выводит на панель экрана сгенерированные числа.
+    /// </summary>
+    /// <param name="message"></param>
+    public void AddNewMessage(string message)
+    {
+        GameObject newTextObject = Instantiate(testObject, transform);
+
+        newTextObject.GetComponent<TextMessage>().SetText(message);
+    }
+    
+    private void OnGUI()
+    {
+        // Отрисовывает кнопку генерации чисел.
+        if (GUI.Button(new Rect(0, 0, 200, 20), "Generate a new number..."))
+        {
+            AddNewMessage(Random.Range(int.MinValue, int.MaxValue).ToString());
+        }
+
+        // Отрисовывает кнопку очистки экрана.
+        if (GUI.Button(new Rect(200, 0, 200, 20), "Clean"))
+        {
+            Clean();
+        }
+    }
+
+    /// <summary>
+    /// Полностью удаляет все сгенерированные значения на панели.
+    /// </summary>
+    public void Clean()
+    {
+        GameObject mainMessage = GameObject.FindWithTag("SpawnMessage");
+
+        if (mainMessage == null)
+        {
+            Debug.LogError("Установит тег SpawnMessage на объект к которому прикреплён сценарий MessageSystem.");
+            Debug.LogError("Аварийное устранение проблемы...");
+
+            mainMessage = GameObject.Find("MessagePanel");
+
+            Debug.LogWarning("Проблема успешно устранена");
+        }
+
+        foreach (Transform item in mainMessage.GetComponentInChildren<RectTransform>())
+        {
+            Destroy(item.GetComponentInChildren<RectTransform>().gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TextMessage.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextMessage : MonoBehaviour
+{
+    internal void SetText(string message)
+    {
+        GetComponent<Text>().text = message;
+    }
+}
